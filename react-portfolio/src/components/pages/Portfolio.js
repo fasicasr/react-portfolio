087d6f71287aa2625ec94b32e16ad829598299d0
Fasica <fasicasr@gmail.com>
2021-07-28 01:37:10 -0400

    add image, assigment discription, links to assignments top portfolio page

--- react-portfolio/src/components/pages/Portfolio.js
+++ react-portfolio/src/components/pages/Portfolio.js
@@ -1,17 +1,90 @@
 import React from 'react';
+import {Row, Card,ListGroup, ListGroupItem} from "react-bootstrap"
+import eye from '../images/eye.jpg';
+import again from '../images/again.png';
 
 export default function Portfolio() {
   return (
     <div>
-      <h1>Portfolio Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      
+    <Row>
+            <Card style={{ width: '25rem' }}>
+        <Card.Img variant="top" src={eye} />
+        <Card.Body>
+          <Card.Title>ViBlocks</Card.Title>
+          <Card.Text>
+          Full stack application. A digital audio workstation that allows users to create music using a step sequencer model. 
+          </Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroupItem>Made with:</ListGroupItem>
+          <ListGroupItem>JavaScript</ListGroupItem>
+          <ListGroupItem>ExpressJS</ListGroupItem>
+          <ListGroupItem>MySQL</ListGroupItem>
+        </ListGroup>
+        <Card.Body>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '25rem' }}>
+        <Card.Img variant="top" src={again} />
+        <Card.Body>
+        <Card.Title>CryptoCurrently</Card.Title>
+          <Card.Text>
+          Full stack application. A digital audio workstation that allows users to create music using a step sequencer model. 
+          </Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroupItem>Made with:</ListGroupItem>
+          <ListGroupItem>JavaScript</ListGroupItem>
+          <ListGroupItem>API</ListGroupItem>
+        </ListGroup>
+        <Card.Body>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+  </Row>
+  <Row>
+            <Card style={{ width: '25rem' }}>
+        <Card.Img variant="top" src={eye} />
+        <Card.Body>
+          <Card.Title>Workout Tracker</Card.Title>
+          <Card.Text>
+          A workout tracking app that allows users to to create, track, and view daily workouts.
+          </Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroupItem>Made with:</ListGroupItem>
+          <ListGroupItem>JavaScript</ListGroupItem>
+          <ListGroupItem>ExpressJS</ListGroupItem>
+          <ListGroupItem>MongoDB</ListGroupItem>
+        </ListGroup>
+        <Card.Body>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '25rem' }}>
+        <Card.Img variant="top" src={again} />
+        <Card.Body>
+          <Card.Title>Weather Dashboard</Card.Title>
+          <Card.Text>
+          An weather tracking application that allowas users to get 5 day forcast based on city.
+          </Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroupItem>Made with:</ListGroupItem>
+          <ListGroupItem>JavaScript</ListGroupItem>
+          <ListGroupItem>API</ListGroupItem>
+        </ListGroup>
+        <Card.Body>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+  </Row>
     </div>
   );
 }
