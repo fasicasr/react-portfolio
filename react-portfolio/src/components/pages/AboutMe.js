55bf332d22c7d7745d6d3fa8517a221a5e72d797
Fasica <fasicasr@gmail.com>
2021-07-27 23:17:26 -0400

    implement contact form with correct fields

--- react-portfolio/src/components/pages/AboutMe.js
+++ react-portfolio/src/components/pages/AboutMe.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import '../styles/AboutMe.css';
+import images from '../images/mypic.png';
+
+import {Container, Row, Col, Image} from "react-bootstrap"
 
 export default function AboutMe() {
   return (
     <div>
-      <h1>About Me page</h1>
+      <h1>Fasica Robi</h1>
+      <Container>
+        <Row>
+          
+          <Col xs={6} md={4}>
+            <Image src={"../images/mypic.png"} roundedCircle />
+          </Col>
+  
+        </Row>
+      </Container>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
+      My background is in education management but after discovering my passion for web development, I couldn’t get enough. I joined the George Washington University Coding Bootcamp to build/strengthen my knowledge through hands-on learning. I’m looking forward to a bright future as a coder.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
     </div>
   );
--- react-portfolio/src/components/pages/Contact.js
+++ react-portfolio/src/components/pages/Contact.js
@@ -1,22 +1,35 @@
 import React from 'react';
+import {Form, Card, Button, InputGroup,FormControl } from "react-bootstrap"
 
 export default function Contact() {
   return (
     <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <h1>Contact Me</h1>
+      <Card style={{ width: "50%" }}>
+        <Card.Header>What to know more?</Card.Header>
+        <Card.Body>
+        <InputGroup className="mb-3">
+          <InputGroup.Text>First and last name</InputGroup.Text>
+          <FormControl aria-label="First name" />
+          <FormControl aria-label="Last name" />
+        </InputGroup>
+        <Form>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="name@example.com" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Message:</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+              </Form.Group>
+            </Form>
+          <Button variant="primary">Submit</Button>
+        </Card.Body>
+   </Card>
+
+      
     </div>
   );
 }
+
+
